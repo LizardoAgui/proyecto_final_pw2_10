3cd3dda925ffb559d70cedba18870dec3c1ee2ce
LizardoAgui <lizardoaguilar100@gmail.com>
2025-07-27 12:11:40 -0600

    Deploy actualizado

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,6 @@ const obtenerEntradas = () => {
 export default defineConfig(
     {
         appType: 'mpa',
-        base: process.env.DEPLOY_BASE_URL,
         build: {
             rollupOptions: {
                 input: obtenerEntradas()
